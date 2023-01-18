def152e2ac745f48b0feddd4444007ec75974db5
Jaime BS <jaimejb.code@gmail.com>
2023-01-18 16:33:08 -0600

    Actualización de urls de API

--- src/helpers/fetchProductosNombre.js
+++ src/helpers/fetchProductosNombre.js
@@ -1,7 +1,7 @@
 export const fetchProductosNombre = (nombre) => {
 
     // const url = `http://localhost:3000/buscar/productos/nombre/${nombre}`;
-    const url = `https://www.api-mejico.jaimejb.com/buscar/productos/nombre/${nombre}`;
+    const url = `https://api-mejico.jaimejb.com/buscar/productos/nombre/${nombre}`;
 
     return fetch(url)
         .then(res => res.json());
